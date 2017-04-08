8a62e8c7ec6e60afd5b95b6d3c996f975d534a6d
madan <madan.dale@gmail.com>
2017-04-08 07:50:38 +0530

    Initial Commit

---
+++ AdminView/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AdminView
+//
+//  Created by Madan Dale on 4/8/17.
+//  Copyright © 2017 Madan Dale. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
